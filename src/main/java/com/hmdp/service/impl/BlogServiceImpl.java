9d595989db9692309251d253ad1fea11c0a50a3a
liyuhan <2690965135@qq.com>
2026-04-08 19:23:09 +0800

    点赞功能实现

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,7 +1,9 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
@@ -9,7 +11,9 @@ import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -27,6 +31,8 @@ import java.util.List;
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
     @Autowired
     private IUserService userService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Result queryBlogById(Long id) {
@@ -35,7 +41,20 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
             return Result.fail("笔记不存在");
         }
         queryBlogUser(blog);
+        //查询是否点赞
+        isBlogLiked(blog);
         return Result.ok(blog);
+    }
+
+    private void isBlogLiked(Blog blog) {
+        UserDTO user = UserHolder.getUser();
+        if (user == null) {
+            // 用户未登录，无需查询是否点赞
+            return;
+        }
+        String key = "blog:liked:" + blog.getId();
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, user.getId().toString());
+        blog.setIsLike(isMember);
     }
 
     private void queryBlogUser(Blog blog) {
@@ -56,7 +75,33 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         // 查询用户
         records.forEach(blog ->{
             queryBlogUser(blog);
+            isBlogLiked(blog);
         });
         return Result.ok(records);
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "blog:liked:" + id;
+        // 判断是否已经点赞
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+        if (BooleanUtil.isFalse(isMember)) {
+            // 没有点赞，可以点赞
+            boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
+            if (isSuccess) {
+                // 保存用户到Redis的Set集合中
+                stringRedisTemplate.opsForSet().add(key, userId.toString());
+            }
+        } else {
+            // 已经点赞，取消点赞
+            boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
+            if (isSuccess) {
+                // 从Redis的Set集合中移除用户
+                stringRedisTemplate.opsForSet().remove(key, userId.toString());
+            }
+
+        }
+        return Result.ok();
     }
 }
