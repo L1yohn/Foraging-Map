489933525fe2f810adf336e72a36d9c55c257bf0
liyuhan <2690965135@qq.com>
2026-04-11 16:12:03 +0800

    附近商户查询

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -16,5 +16,7 @@ public interface IShopService extends IService<Shop> {
 
     Result queryById(Long id);
 
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
+
     Result update(Shop shop);
 }
